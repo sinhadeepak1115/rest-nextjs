a3084437295934fb4346da49e99ea6a41435a6db
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-09-24 19:20:10 +0530

    added get handler

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,5 +1,16 @@
 import { NextResponse } from "next/server";
+import User from "@/lib/modals/user";
+import connectDB from "@/lib/db";
 
-export const GET = () => {
-  return NextResponse.json({ message: "Hello World!" });
+export const GET = async () => {
+  try {
+    await connectDB();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+    //eslint-disable-next-line
+  } catch (error: any) {
+    return new NextResponse("Error in fetching users" + error.message, {
+      status: 500,
+    });
+  }
 };
